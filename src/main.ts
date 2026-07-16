511a8fcca64e509be64becfd7f7c9ba2d40d1b45
davilopes <davilopesrdgs@gmail.com>
2026-07-16 08:38:24 -0300

    feat: configuracao de cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
