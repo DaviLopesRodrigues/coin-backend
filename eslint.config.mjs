28711ad55775c2ed37649ed084e3e0c577d19d04
davilopes <davilopesrdgs@gmail.com>
2026-07-27 21:58:44 -0300

    feat: criacao de lint para imports inutilizados

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { UseFilters } from '@nestjs/common';
 
 export default tseslint.config(
   {
@@ -24,12 +26,29 @@ export default tseslint.config(
       },
     },
   },
+
   {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 );
